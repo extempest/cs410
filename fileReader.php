4ab38618c3de1ca89bf25299f5cd84c5da0f289f
Daniel Kang <gwanggo1@hotmail.com>
2014-10-20 17:28:46 -0700

    not done. still in progress

--- fileReader.php
+++ fileReader.php
@@ -22,19 +22,19 @@ $(document).ready(function() {
                   echo $file
                   ?>
                   // in order to make txt file, put :  git log -p --reverse > txtfileName.txt
-
-                   console.log(test);
-                   console.log(test[2]);
                   
-                   var test2 = ["apple","orange","grapes"];
-                   console.log(test2);
-                   console.log(test2[2]);
+                  
+                  console.log(test);
+                  console.log(test[2]);
+                  
+                  var test2 = ["apple","orange","grapes"];
+                  console.log(test2);
+                  console.log(test2[2]);
                   
                   
                   document.getElementById("outputjs1").innerHTML = test;
                   document.getElementById("outputjs2").innerHTML = test2;
-});
-
+                  });
 
 </script>
 </head>
@@ -58,17 +58,72 @@ $(document).ready(function() {
 <div>output from js:</div>
 <div id="outputjs1"></div>
 <div id="outputjs2"></div>
+
+<?php
+    $handle = @fopen("mockByengProject.txt","r");
+    if($handle) {
+        $commitIndex = -1;
+        while (($buffer = fgets($handle)) !== false){
+            $temp = str_word_count($buffer,1,'1234567890-+');
+            //$lineTemp = str_split($buffer);
+            print_r($temp);
+            echo "<br/>";
+            if($temp[0] == "commit"){
+                if($commitIndex != -1){
+                    print_r($commitIndex);
+                    $commit = array($author, $email, $commitTime ,$commitDay, $commitMonth, $commitYear);
+                    print_r($commit);
+                }
+                $commitIndex++;
+            }
+            if($temp[0] == "Author"){
+                $authorBuffer = str_word_count($buffer,1,'1234567890@.');
+                //print_r($lineBuffer);
+                $author = $authorBuffer[1];
+                $email = $authorBuffer[2];
+            }
+            if($temp[0] == "Date"){
+                $dateBuffer = str_word_count($buffer,1,'1234567890:-');
+
+                $commitMonth = $dateBuffer[2];
+                $commitDay = $dateBuffer[3];
+                $commitTime = $dateBuffer[4];
+                $commitYear = $dateBuffer[5];
+            }
+            if($temp[0] == "---" || $temp[0] == "+++"){ //file is removed, modified, or created
+                $fileNameinArray = $temp;
+                end($fileNameinArray);
+                $endIndex = key($fileNameinArray);
+//                if($temp[$endIndex] = "java"){
+//                    
+//                }
+            }
+        }
+        if(!feof($handle)){
+            echo "Error:unexpected fgets() fail\n";
+        }
+        
+        //This is for the last commit. Since I am creating $commit when a word "commit" is appeared, and there is no word "commit" at the end of file.
+        $commit = array($author, $email, $commitTime, $commitDay, $commitMonth, $commitYear);
+        $commitIndex++;
+        print_r($commit);
+        
+        fclose($handle);
+    }
+    ?>
+
+
 <script>
 
 window.onload =function() {
     
     var test2 = ["apple","orange","grapes"];
-
+    
     //$("#outputjs1").appendChild("test");
     //$("#outputjs2").appendCHild(test2);
     //console.log($("#outputjs1").innerHTML);
     //console.log($("#outputjs2").innerHTML);
-
+    
 }
 </script>
 </body>
