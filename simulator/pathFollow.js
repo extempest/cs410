eb7b21fa5390e402d96fec33c889091e3d6a5588
BoRa Choi <boradori_@hotmail.com>
2014-10-23 18:06:17 -0700

    pathFollow - orbit

--- simulator/pathFollow.js
+++ simulator/pathFollow.js
@@ -7,7 +7,7 @@ function ready(error, xml) {
     //mockData
     //var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
     
-    var groundLevel = 100;
+    var groundLevel = 200;
     var data = [
                 {
                     'date':new Date(2014, 0, 1, 0, 0, 0, 0),
@@ -58,6 +58,14 @@ function ready(error, xml) {
     var svg = d3.select("svg")
     .attr("width",window.innerWidth)
     .attr("height",window.innerHeight);
+    
+    var sky = createBackground(svg);
+    var sun = svg.append("circle")
+    .attr("r", 40)
+    .style("fill", "yellow");
+    var moon = svg.append("circle")
+    .attr("r", 30)
+    .style("fill", "white");
 
     
     //var path = svg.select("path#wiggle"),
@@ -95,6 +103,26 @@ function ready(error, xml) {
             //console.log()
             var intervalId = setInterval(function() {
                                          todayLabel.text(today)
+                                         if(today.getHours()==0||today.getHours()==12){
+                                            sky.transition()
+                                            .duration(12000)
+                                            .style("fill", skycolor(today.getHours()));
+                                         }
+                                         
+                                         if(today.getHours()==0){
+                                         sun
+                                         .transition()
+                                         .duration(24000)
+                                         .ease("in-out")
+                                         .attrTween("transform", orbit(1/4));
+                                         moon
+                                         .transition()
+                                         .duration(24000)
+                                         .ease("in-out")
+                                         .attrTween("transform", orbit(3/4));
+                                         
+                                         }
+                                         
                                          //console.log(today.getTime());
                                          if (today.getTime() < tommorrow.getTime()){
                                          commits.forEach(function(commit){
@@ -112,19 +140,18 @@ function ready(error, xml) {
                                                          
                                                                      var circ = createAnt(svg);
                                                                      author["antMarker"] = circ;
-                                                                     circ
-                                                                     .attr("transform", "translate(" + [7,groundLevel] + ")")
-                                                                     ;
+                                                         
+                                                         
                                                          
                                                                      
                                                                      console.log(circ);
                                                                      circ.transition()
                                                                      .duration(500)
-                                                                     .attr("transform", "translate(" + [27,groundLevel] + ")")
+                                                                     .attr("transform", "translate(" + [27,groundLevel-55] + ")")
                                                                      
                                                                      .transition()
                                                                      .duration(500)
-                                                                     .attr("transform", "translate(" + [Math.random()*500,Math.random()*500] + ")");
+                                                                     .attr("transform", "translate(" + [Math.random()*500,groundLevel-55] + ")");
                                                                      
                                                                     authors[commit["author"]] = author;
                                                          
@@ -134,7 +161,7 @@ function ready(error, xml) {
                                                                     var circ = author["antMarker"]
                                                                      circ.transition()
                                                                      .duration(500)
-                                                                     .attr("transform", "translate(" + [Math.random()*500,Math.random()*500] + ")");
+                                                                     .attr("transform", "translate(" + [Math.random()*500,groundLevel-55] + ")");
                                                                      
                                                                      
                                                                     }
@@ -159,6 +186,7 @@ function ready(error, xml) {
     
     
     
+    
        //Get path start point for placing marker
     function pathStartPoint(path) {
         var d = path.attr("d"),
@@ -174,6 +202,8 @@ function ready(error, xml) {
     }
     
     function createAnt(canvas) {
+        //Height of an ant
+        var height = 55;
         
         //Initialize random color
         var color = "hsl(" + Math.random() * 360 + ",100%,50%)";
@@ -181,11 +211,11 @@ function ready(error, xml) {
         
         // Draw the Circle
         var circleData = [
-                          { "cx": 80, "cy": 80, "radius": 20, "color": color  },
-                          { "cx": 110, "cy": 80, "radius": 20, "color": color },
-                          { "cx": 140, "cy": 80, "radius": 20, "color": color },
-                          { "cx": 147, "cy": 73, "radius": 6, "color": "white"  },
-                          { "cx": 147, "cy": 73, "radius": 3, "color": color  } ];
+                          { "cx": 20, "cy": 20, "radius": 20, "color": color  },
+                          { "cx": 50, "cy": 20, "radius": 20, "color": color },
+                          { "cx": 80, "cy": 20, "radius": 20, "color": color },
+                          { "cx": 87, "cy": 15, "radius": 6, "color": "white"  },
+                          { "cx": 87, "cy": 15, "radius": 3, "color": color  } ];
         
     
         
@@ -207,39 +237,175 @@ function ready(error, xml) {
         
         // draw legs in group1
         var line = group1.append("line")
-        .attr("x1", 73)
-        .attr("y1", 80)
-        .attr("x2", 73)
-        .attr("y2", 115)
+        .attr("x1", 13)
+        .attr("y1", 20)
+        .attr("x2", 13)
+        .attr("y2", 55)
         .attr("stroke-width", 2)
         .attr("stroke", color);
         var line = group1.append("line")
-        .attr("x1", 85)
-        .attr("y1", 80)
-        .attr("x2", 85)
-        .attr("y2", 115)
+        .attr("x1", 25)
+        .attr("y1", 20)
+        .attr("x2", 25)
+        .attr("y2", 55)
         .attr("stroke-width", 2)
         .attr("stroke", color);
         var line = group1.append("line")
-        .attr("x1", 120)
-        .attr("y1", 80)
-        .attr("x2", 120)
-        .attr("y2", 115)
+        .attr("x1", 60)
+        .attr("y1", 20)
+        .attr("x2", 60)
+        .attr("y2", 55)
         .attr("stroke-width", 2)
         .attr("stroke", color);
         var line = group1.append("line")
-        .attr("x1", 132)
-        .attr("y1", 80)
-        .attr("x2", 132)
-        .attr("y2", 115)
+        .attr("x1", 72)
+        .attr("y1", 20)
+        .attr("x2", 72)
+        .attr("y2", 55)
         .attr("stroke-width", 2)
         .attr("stroke", color);
         
-
-                     
+        group1
+        .attr("transform", "translate(" + [7,groundLevel-height] + ")");
+        
         
         return group1;
 
+    }
+    
+    function createBackground(canvas) {
+        var rectangle = canvas.append("rect")
+                            .attr("x", 0)
+                            .attr("y", groundLevel)
+                            .attr("width", window.innerWidth)
+                            .attr("height", window.innerHeight-groundLevel)
+        .style("fill", "#A9672E");
+        
+        var sky = canvas.append("rect")
+                        .attr("x", 0)
+                        .attr("y", 0)
+                        .attr("width", window.innerWidth)
+        .attr("height", groundLevel)
+        .style("fill", "#0c1317");
+        
+        
+        
+        return sky;
+    }
+    
+    function skycolor(hours) {
+        var skycolor = "blue"
+        switch (hours) {
+            case 0:
+                console.log("its getting brighter");
+                skycolor = "#97ccf1";
+                
+                break;
+            case 12:
+                skycolor = "#0c1317";
+                break;
+                
+                
+                /*
+        case 0:
+            skycolor = "#00000";
+                break;
+        case 1:
+                skycolor = "#0c1317";
+                break;
+        case 2:
+                skycolor = "#19262f";
+                break;
+        case 3:
+                skycolor = "#253947";
+                break;
+        case 4:
+                skycolor = "#324c5f";
+                break;
+        case 5:
+                skycolor = "#3f6077";
+                break;
+        case 6:
+                skycolor = "#4b738e";
+                break;
+        case 7:
+                skycolor = "#5886a6";
+                break;
+        case 8:
+                skycolor = "#6499be";
+                break;
+        case 9:
+                skycolor = "#71acd6";
+                break;
+        case 10:
+                skycolor = "#7ec0ee";
+                break;
+        case 11:
+                skycolor = "#8ac6ef";
+                break;
+        case 12:
+                skycolor = "#97ccf1";
+                break;
+        case 13:
+                skycolor = "#8ac6ef";
+                break;
+        case 14:
+                skycolor = "#7ec0ee";
+                break;
+        case 15:
+                skycolor = "#71acd6";
+                break;
+        case 16:
+                skycolor = "#6499be";
+                break;
+        case 17:
+                skycolor = "#5886a6";
+                break;
+        case 18:
+                skycolor = "#4b738e";
+                break;
+        case 19:
+                skycolor = "#3f6077";
+                break;
+        case 20:
+                skycolor = "#324c5f";
+                break;
+        case 21:
+                skycolor = "#253947";
+                break;
+        case 22:
+                skycolor = "#19262f";
+                break;
+        case 23:
+                skycolor = "#0c1317";
+                break;
+        case 24:
+                skycolor = "#00000";
+                break;
+        */
+            
+        }
+        return skycolor;
+    }
+
+    
+    function orbit(initValue) {
+        
+        return function(d, i, a) {
+            return function(t) {
+
+                var t_x, t_y;
+                
+                var rotation_radius_x = (window.innerWidth/2)+80;
+                var rotation_radius_y = 200;
+                var t_angle = (2 * Math.PI) * (t+(initValue));
+                var t_x = rotation_radius_x * Math.cos(t_angle);
+                var t_y = rotation_radius_y * Math.sin(t_angle);
+                
+                
+                return "translate(" + ((window.innerWidth/2) + t_x) + "," + (200 + t_y) + ")";
+            };
+        };
     }
     
     function translateAlong(path) {
